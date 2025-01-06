95c837176b4ee2a12afc03bd3eb69e29b139b01a
This-Is-Ram <durgaramannapureddy@gmail.com>
2025-01-06 18:12:47 +0530

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-
-import products from "../api/products.json";
-import { showProductContainer } from "../homeProductCards";
-// console.log(products);
-// let { brand, category, description, id, image, name, price, stock } = products;
-// calling a function to show card values etc..
-showProductContainer(products);
